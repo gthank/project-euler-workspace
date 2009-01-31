a0a42b908cd1bc45662eb7a30bc267523c13d69e
gthank <hank.gay@gmail.com>
2009-01-31 16:53:20 -0500

    Fixed a problem with the filter condition in my list comprehension (I was filtering even indexes, not even Fibonacci numbers)

--- problem2.py
+++ problem2.py
@@ -15,7 +15,8 @@ def problem_two():
     """Calculate the sum of the even Fibonacci numbers less than 4,000,000."""
     # I precalculated the index of the largest Fibonacci number less than
     # 4,000,000 and it was 32.
-    return sum([fib(n) for n in range(33) if not n % 2])
+    fibs = [fib(n) for n in range(33)]
+    return sum([cur_fib for cur_fib in fibs if not cur_fib % 2])
 
 if (__name__ == "__main__"):
     print problem_two()
