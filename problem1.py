a35be383340de97b312935ee45afa84531f34cab
gthank <hank.gay@gmail.com>
2009-01-31 16:01:01 -0500

    Fixed an error in my calculations (it turns out range() is non-inclusive)

--- problem1.py
+++ problem1.py
@@ -6,7 +6,7 @@ For more details, see http://projecteuler.net/index.php?section=problems&id=1
 
 def problem_one():
     """Sum the natural numbers less than 1000 that are multiples of 3 or 5."""
-    return sum([n for n in range(999) if ((0 == n % 3) or (0 == n % 5))])
+    return sum([n for n in range(1000) if ((0 == n % 3) or (0 == n % 5))])
     
 
 if (__name__ == "__main__"):
