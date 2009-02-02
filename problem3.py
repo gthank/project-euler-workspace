515931fd733b398597008919756f1ad91a3ee6f1
gthank <hank.gay@gmail.com>
2009-02-01 22:36:43 -0500

    Went iterative instead of tail-recursive (too much recursion errors? Lame.)

--- problem3.py
+++ problem3.py
@@ -4,17 +4,14 @@ import math
 
 def e_sieve(upper_bound):
     """Uses the Sieve of Eratosthenes to get a list of the primes up to max."""
-    return _e_sieve_helper([], range(2, int(math.sqrt(upper_bound))))
+    primes = []
+    candidates = range(2, int(math.sqrt(upper_bound)))
+    while candidates:
+        head = candidates[0]
+        primes.append(head)
+        candidates = [n for n in candidates[1:] if n % head]
 
-def _e_sieve_helper(primes, candidates):
-    """Find the primes in candidates using the Sieve of Eratosthenes."""
-    if not candidates:
-        return primes
-
-    head = candidates[0]
-    primes.append(head)
-    candidates = [n for n in candidates[1:] if n % head]
-    return _e_sieve_helper(primes, candidates)
+    return primes
 
 def find_highest_prime_factor(to_factor):
     """Find the highest prime factor of to_factor."""
