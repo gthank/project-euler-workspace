a0ae4351371dcce8c5d239df74dba2909e45ea39
gthank <hank.gay@gmail.com>
2009-02-04 06:45:06 -0500

    Added the script; left myself breadcrumbs

---
+++ problem5.py
@@ -0,0 +1,6 @@
+"""Solves Problem 5 of Project Euler."""
+
+if __name__ == '__main__':
+    # Basic idea: factor the numbers; reduce to a tuple for each atom so [2, 2, 3] -> [(2,2), (3,1)]
+    # Multiply the unique factors together using the highest degree
+    print "Work in progress"
