6c6dd959df1d0096b6444310850126704aba48e2
gthank <hank.gay@gmail.com>
2009-02-05 05:09:04 -0500

    Updated sum_of_squares to use a mathematical shortcut I just learned.

--- problem6.py
+++ problem6.py
@@ -3,7 +3,9 @@
 def sum_of_squares(upper_bound):
     """Sums the squares of all the natural numbers from 1 to \
     upper_bound (inclusive)."""
-    return sum(n ** 2 for n in range(1, upper_bound + 1))
+    # Use the rule for square pyramidal numbers
+    # (http://en.wikipedia.org/wiki/Square_pyramidal_number).
+    return ((2 * upper_bound ** 3) + (3 * upper_bound ** 2) + upper_bound) / 6
 
 def square_of_sums(upper_bound):
     """Sums all the numbers from 1 to upper_bound."""
