51f07ae6e1deb11c441787f9be803d07a0539f20
gthank <hank.gay@gmail.com>
2009-02-06 06:31:23 -0500

    Switched to a generator-based solution that is WAY faster.

--- problem7.py
+++ problem7.py
@@ -1,46 +1,40 @@
 """Solves Problem 7 from Project Euler."""
 
 import math
+import sys
 
-FIRST_SIX_PRIMES = {1:2, 2:3, 3:5, 4:7, 5:11, 6:13}
+def is_prime(candidate, known_primes):
+    """Determines whether candidate is prime by trial division using \
+    known_primes.
 
-def e_sieve(upper_bound):
-    """Uses the Sieve of Eratosthenes to get a list of the primes up to max."""
+    For this function to work, known_primes *must* be accurate.
+    """
+    last_possible = math.sqrt(candidate)
+    for current_prime in known_primes:
+        if current_prime > last_possible:
+            break
+        if not candidate % current_prime:
+            return False
+    return True
+
+def primes_generator():
+    """A generator for all the primes that fit in xrange."""
+    candidates = xrange(2, sys.maxint)
     primes = []
-    candidates = range(2, upper_bound)
-    while candidates:
-        head = candidates[0]
-        primes.append(head)
-        candidates = [n for n in candidates[1:] if n % head]
-
-    return primes
-
-# I know it's currently unused, but I wanted to write a generator.
-def primes_generator(upper_bound):
-    """Uses the Sieve of Eratosthenes behind the scenes to build a \
-    generator for prime numbers."""
-    candidates = range(2, upper_bound)
-    while len(candidates) > 1:
-        next_prime = candidates.pop(0)
-        candidates = [n for n in candidates if n % next_prime]
-        yield next_prime
-    yield candidates.pop()
+    for n in candidates:
+        if is_prime(n, primes):
+            primes.append(n)
+            yield n
 
 def problem_7(n):
-    """Finds the nth prime number.
+    """Finds the nth prime number."""
+    primes = primes_generator()
+    i = 0
+    while i < n - 1:
+        i += 1
+        primes.next()
 
-    Thanks to
-    http://en.wikipedia.org/wiki/Prime_number_theorem#Approximations_for_the_nth_prime_number
-    we know that it will be less than n * ln(n) + n * ln(ln(n))
-    (for n >= 6).
-    """
-    if n < 6:
-        return FIRST_SIX_PRIMES[n]
-
-    upper_bound = int(n * math.log(n) + n * math.log(math.log(n)))
-
-    primes = e_sieve(upper_bound)
-    return primes[n - 1]
+    return primes.next()
 
 if __name__ == '__main__':
     print problem_7(10001)
