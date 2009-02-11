87574d4e028cfcc262f6620cdfd63679e34d7e8b
gthank <hank.gay@gmail.com>
2009-02-10 21:12:37 -0500

    Initial script for solving the problem

---
+++ problem9.py
@@ -0,0 +1,8 @@
+"""Solves Problem 9 from Project Euler."""
+
+def problem_9():
+    """Finds the product of the Pythagorean Triplet where a + b + c = 1000."""
+    return 0
+
+if __name__ == '__main__':
+    print problem_9()
