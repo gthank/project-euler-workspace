620c3ad654986085dc4d89873e5ce4f0e2e4df67
gthank <hank.gay@gmail.com>
2009-02-10 21:43:19 -0500

    Put an upper bound on the generator and made it more complete

--- problem9.py
+++ problem9.py
@@ -2,25 +2,24 @@
 
 import operator
 
-def triplets():
+def triples(upper_bound):
     """Generator for Pythagorean Triples (represented as tuples).
 
     Uses Euclid's formula to generate Pythagorean Triples
-    (see http://en.wikipedia.org/wiki/Pythagorean_triple#Generating_a_triple)."""
-    m = 2
-    n = 1
-    while True:
-        yield m ** 2 - n ** 2, 2 * m * n, m ** 2 + n ** 2
-        m += 1
-        n += 1
-
+    (see http://en.wikipedia.org/wiki/Pythagorean_triple#Generating_a_triple).
+    """
+    for m in xrange(2, upper_bound):
+        for n in xrange(1, m):
+            yield m ** 2 - n ** 2, 2 * m * n, m ** 2 + n ** 2
+# Only uncomment if the triples we get from the original Euclid's are insufficient.
+# for k in xrange(1, upper_bound):
+#     yield k * (m ** 2 - n ** 2), k * (2 * m * n), k * (m ** 2 + n ** 2)
+                
 def problem_9():
     """Finds the product of the Pythagorean Triple where a + b + c = 1000."""
-    for triplet in triplets():
-        if sum(triplet) == 1000:
-            return reduce(operator.mul, triplet)
-        elif sum(triplet) > 1000:
-            break
+    for triple in triples(1000):
+        if sum(triple) == 1000:
+            return reduce(operator.mul, triple)
     return 0
 
 if __name__ == '__main__':
