89f772bb6a8aee50087cc936904bec59af6b7011
gthank <hank.gay@gmail.com>
2009-02-17 04:36:53 -0500

    Initial creation of script to solve the problem.

---
+++ problem12.py
@@ -0,0 +1,8 @@
+"""Solves Problem 12 of Project Euler."""
+
+def problem_12(min_divisors):
+    """Finds the first triangular number to have more than 500 divisors."""
+    pass
+
+if __name__ == '__main__':
+    print problem_12(500)
