aca4b41547452021efa6cd1aa5b68a30e4d5a8c4
gthank <hank.gay@gmail.com>
2009-02-17 04:55:43 -0500

    Stubbing in the helper functions.

--- problem12.py
+++ problem12.py
@@ -1,5 +1,29 @@
 """Solves Problem 12 of Project Euler."""
 
+import math
+
+def nonprime_factors(to_factor):
+    """Find the factors of to_factor."""
+    factors = []
+    divisor = 1
+    while (divisor <= int(math.sqrt(to_factor))):
+        if not to_factor % divisor:
+            to_factor /= divisor
+            factors.append(divisor)
+            factors.append(to_factor)
+        divisor += 1
+
+    return factors
+
+def triangular_numbers():
+    """Generate the triangular numbers."""
+    current = 0
+    position = 1
+    while True:
+        current += position
+        position += 1
+        yield current
+
 def problem_12(min_divisors):
     """Finds the first triangular number to have more than 500 divisors."""
     pass
