42961d5cf1157d762451f0a257bc1e8836977380
gthank <hank.gay@gmail.com>
2009-03-29 12:30:44 -0400

    Added a verion that does string conversion so we can treat the num as a sequence of digits.

--- problem16.py
+++ problem16.py
@@ -1,10 +1,13 @@
 import math
+import operator
 
-def sum_of_digits(num):
+def pure_numeric_sum_of_digits(num):
     """
     Finds the sum of the digits in num (presumed to be base-10).
 
-    >>> sum_of_digits(2 ** 15)
+    This version of the method operates purely on numbers.
+
+    >>> pure_numeric_sum_of_digits(2 ** 15)
     26
     """
     running_total = 0
@@ -13,9 +16,23 @@ def sum_of_digits(num):
         num //= 10
     return running_total
 
+def string_based_sum_of_digits(num):
+    """
+    Finds the sum of the digits in num (presumed to be base-10).
+
+    This version of the method works by using a string representation
+    of num and converting individual characters to numbers so we can
+    operate on it as a sequence.
+
+    >>> string_based_sum_of_digits(2 ** 15)
+    26
+    """
+    digits = [int(digit) for digit in str(num)]
+    return reduce(operator.add, digits, 0)    
+
 def problem_16():
     """Solves Problem 16 at Project Euler."""
-    return sum_of_digits(2 ** 1000)
+    return pure_numeric_sum_of_digits(2 ** 1000)
 
 if __name__ == '__main__':
     print problem_16()
