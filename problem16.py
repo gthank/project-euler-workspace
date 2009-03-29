5b3b81c14720aa1d6c77a15ff95a240bcb0aaea6
gthank <hank.gay@gmail.com>
2009-03-29 12:19:52 -0400

    First stab at problem 16

---
+++ problem16.py
@@ -0,0 +1,21 @@
+import math
+
+def sum_of_digits(num):
+    """
+    Finds the sum of the digits in num (presumed to be base-10).
+
+    >>> sum_of_digits(2 ** 15)
+    26
+    """
+    running_total = 0
+    for n in xrange(math.log10(num) + 1):
+        running_total += num % 10
+        num //= 10
+    return running_total
+
+def problem_16():
+    """Solves Problem 16 at Project Euler."""
+    return sum_of_digits(2 ** 1000)
+
+if __name__ == '__main__':
+    print problem_16()
