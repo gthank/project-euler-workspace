0d778f4a42669c27947d43a03a55c9a989bb7066
gthank <hank.gay@gmail.com>
2009-12-03 17:13:47 -0500

    First attempt to actually solve the problem.

--- problem25.py
+++ problem25.py
@@ -12,3 +12,14 @@ def fib():
     while True:
         yield a
         a, b = b, a+b
+
+
+def problem_twenty_six():
+    """Find the first term in the Fibonacci sequence to contain 1000 digits."""
+    fibs = fib()
+    big_num = 10**999
+    return itertools.dropwhile(lambda x: len(str(x)) < big_num, fibs).next()
+
+
+if (__name__ == "__main__"):
+    print problem_twenty_six()
